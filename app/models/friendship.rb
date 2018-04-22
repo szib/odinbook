c495d45bc2c1413c0cbb158906983b7b5b227fce
Ivan Szebenszki <szib@users.noreply.github.com>
2018-04-22 21:11:30 +0100

    Add uniqueness to friendship

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -5,6 +5,9 @@ class Friendship < ApplicationRecord
   belongs_to :user
   belongs_to :friend, class_name: 'User'
 
+  validates_presence_of :user, :friend
+  validates_uniqueness_of :user, scope: :friend
+
   private
 
   def create_inverse_friendship
