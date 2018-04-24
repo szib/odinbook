f3b1da4d675f3527122bf59ba7b3b5f71a477d82
Ivan Szebenszki <szib@users.noreply.github.com>
2018-04-24 17:10:05 +0100

    Add user#index and user#show

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/show'
   # devise_for :users
   devise_for :users, controllers: { registrations: 'users/registrations' }
+  resources :users, only: [:index, :show]
 
   get 'static_pages/home'
   get 'static_pages/help'
