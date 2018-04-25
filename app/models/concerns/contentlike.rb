b9689096a22ad231f2541a72125c48f9023e2320
Ivan Szebenszki <szib@users.noreply.github.com>
2018-04-25 19:31:59 +0100

    Add content length restriction

--- app/models/concerns/contentlike.rb
+++ app/models/concerns/contentlike.rb
@@ -5,6 +5,7 @@ module Contentlike
     belongs_to :author, class_name: 'User'
     validates_presence_of :author
     validates_presence_of :content
+    validates :content, length: { minimum: 2, maximum: 1000 }
   end
 
 end
