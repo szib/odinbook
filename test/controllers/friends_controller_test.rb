ecc2d48207e7fcc60a641828eb1d91d73e0a10a3
Ivan Szebenszki <szib@users.noreply.github.com>
2018-04-28 23:24:50 +0100

    Removes minitest

--- test/controllers/friends_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require 'test_helper'
-
-class FriendsControllerTest < ActionDispatch::IntegrationTest
-  test "index should render" do
-    get friends_url
-    assert_redirected_to new_user_session_path
-  end
-end
