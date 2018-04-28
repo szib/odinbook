d4373f706d94f7e4f96746d1feb8296c7c6a42a9
Ivan Szebenszki <szib@users.noreply.github.com>
2018-04-28 18:57:07 +0100

    Refactor Befriendable Move uniqueness check back to actual models

--- app/models/friend_request.rb
+++ app/models/friend_request.rb
@@ -1,19 +1,15 @@
 class FriendRequest < ApplicationRecord
   include Befriendable
 
-  validate :not_friends
   validate :no_incoming_request
 
-  def not_friends
-    if user.friend_of?(friend)
-      errors.add(:friend, "#{friend.profile.full_name} is already your friend...")
-    end
-  end
+  validates_uniqueness_of :user, scope: :friend,
+          message: 'You already sent that person a friend request.'
 
   def no_incoming_request
-    if user.requesting_friends.include?(friend)
-      errors.add(:friend,
-                 "#{friend.profile.full_name} has already sent you a friend request.")
+    if user.has_incoming_friend_request_from?(friend)
+      errors.add(:user,
+                 "That person has already sent you a friend request.")
     end
   end
 
