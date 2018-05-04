ef8be80588c6e39ce4145e996bf8d95135f4f3e0
Ivan Szebenszki <szib@users.noreply.github.com>
2018-05-04 10:40:43 +0100

    Implement showing comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,46 +2,48 @@ class CommentsController < ApplicationController
   before_action :set_comment, only: [:show, :edit, :update, :destroy]
 
   # GET /comments
-  def index
-    @comments = Comment.all
-  end
-
-  # GET /comments/1
-  def show
-  end
+  # def index
+  #   @comments = Comment.all
+  # end
+  #
+  # # GET /comments/1
+  # def show
+  # end
 
   # GET /comments/new
   def new
-    @comment = Comment.new
+    # @post 
+    @comment = Comment.new(author: current_user, post: @post)
   end
 
   # GET /comments/1/edit
-  def edit
-  end
+  # def edit
+  # end
 
   # POST /comments
   def create
-    @comment = Comment.new(comment_params)
-
-    if @comment.save
-      redirect_to @comment, notice: 'Comment was successfully created.'
-    else
-      render :new
-    end
+    redirect_back(fallback_location: root_path)
+    # @comment = Comment.new(comment_params)
+    #
+    # if @comment.save
+    #   redirect_to @comment, notice: 'Comment was successfully created.'
+    # else
+    #   render :new
+    # end
   end
 
   # PATCH/PUT /comments/1
-  def update
-    if @comment.update(comment_params)
-      redirect_to @comment, notice: 'Comment was successfully updated.'
-    else
-      render :edit
-    end
-  end
+  # def update
+  #   if @comment.update(comment_params)
+  #     redirect_to @comment, notice: 'Comment was successfully updated.'
+  #   else
+  #     render :edit
+  #   end
+  # end
 
   # DELETE /comments/1
   def destroy
-    @comment.destroy
+    # @comment.destroy
     redirect_to comments_url, notice: 'Comment was successfully destroyed.'
   end
 
