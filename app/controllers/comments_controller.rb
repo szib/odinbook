5bc589b95493c315adfb16bd7d5b07e6779753ef
Ivan Szebenszki <szib@users.noreply.github.com>
2018-05-07 21:35:54 +0100

    Implement create comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,60 +1,24 @@
 class CommentsController < ApplicationController
-  before_action :set_comment, only: [:show, :edit, :update, :destroy]
-
-  # GET /comments
-  # def index
-  #   @comments = Comment.all
-  # end
-  #
-  # # GET /comments/1
-  # def show
-  # end
-
-  # GET /comments/new
-  def new
-    # @post 
-    @comment = Comment.new(author: current_user, post: @post)
-  end
-
-  # GET /comments/1/edit
-  # def edit
-  # end
 
   # POST /comments
   def create
+    @comment = Comment.new(comment_params)
+    @comment.post = Post.find(params[:post_id])
+    @comment.author = current_user
+
+    @comment.save
     redirect_back(fallback_location: root_path)
-    # @comment = Comment.new(comment_params)
-    #
+
     # if @comment.save
-    #   redirect_to @comment, notice: 'Comment was successfully created.'
+    #   redirect_back(fallback_location: root_path)
     # else
     #   render :new
     # end
   end
 
-  # PATCH/PUT /comments/1
-  # def update
-  #   if @comment.update(comment_params)
-  #     redirect_to @comment, notice: 'Comment was successfully updated.'
-  #   else
-  #     render :edit
-  #   end
-  # end
-
-  # DELETE /comments/1
-  def destroy
-    # @comment.destroy
-    redirect_to comments_url, notice: 'Comment was successfully destroyed.'
-  end
-
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_comment
-      @comment = Comment.find(params[:id])
-    end
-
     # Only allow a trusted parameter "white list" through.
     def comment_params
-      params.fetch(:comment, {})
+      params.require(:comment).permit(:content)
     end
 end
--- spec/requests/comments_spec.rb
+++ spec/requests/comments_spec.rb
@@ -1,10 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe "Comments", type: :request do
-  let(:a_comment) { create(:comment) }
+  let(:a_post) { create(:post) }
+  let(:a_comment) { attributes_for(:comment) }
   describe "POST /posts/:post_id/comments" do
     it "works" do
-      post post_comments_path({ post_id: a_comment.post.id})
+      post post_comments_path( post_id: a_post.id, comment: a_comment)
       expect(response).to have_http_status(302)
     end
   end
