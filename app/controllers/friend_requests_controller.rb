27f9a99bb7e5cf25d25d1b57584451ca58e6eca1
Ivan Szebenszki <szib@users.noreply.github.com>
2018-05-21 13:00:23 +0100

    Fix friend_request#create Accepting FR if other user already sent a FR

--- app/controllers/friend_requests_controller.rb
+++ app/controllers/friend_requests_controller.rb
@@ -1,20 +1,22 @@
 class FriendRequestsController < ApplicationController
+  include FriendRequestsHelper
+
   before_action :authenticate_user!
   before_action { find_friend(friend_request_params) }
 
   # Send friend request
   def create
-    current_user.requested_friends << @friend unless @friend.nil?
+    if current_user.has_incoming_friend_request_from?(@friend)
+      accept_friend_request(@friend)
+    else
+      current_user.requested_friends << @friend unless @friend.nil?
+    end
     redirect_to users_path
   end
 
   # Accept friend request
   def update
-    ActiveRecord::Base.transaction do
-      current_user.requested_friends.delete @friend
-      current_user.requesting_friends.delete @friend
-      current_user.friends << @friend
-    end
+    accept_friend_request(@friend)
     redirect_to users_path
   end
 
