a1eca09dec7d57910b754c6a183000537a4a1bc3
Ivan Szebenszki <szib@users.noreply.github.com>
2018-05-24 08:11:10 +0100

    Add welcome mail test

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -1,5 +1,19 @@
 require "rails_helper"
 
 RSpec.describe UserMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe 'wecome mail' do
+    let(:recipient) { create(:user) }
+    let(:welcome_mail) { UserMailer.with(user: recipient).welcome_mail }
+
+    it 'has a correct recipient address' do
+      expect(welcome_mail.to).to eq [recipient.email]
+    end
+
+    it 'renders the body' do
+      expect(welcome_mail.body.encoded).to match "Welcome to Odinbook"
+      expect(welcome_mail.body.encoded).to match recipient.profile.full_name
+      expect(welcome_mail.body.encoded).to match "localhost"
+    end
+  end
 end
