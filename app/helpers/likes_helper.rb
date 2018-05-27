fc4c1d153d8d21a888cbe709bbdd83ccb48ad292
Ivan Szebenszki <szib@users.noreply.github.com>
2018-05-27 13:16:49 +0100

    Style up posts

--- app/helpers/likes_helper.rb
+++ app/helpers/likes_helper.rb
@@ -1,14 +1,14 @@
 module LikesHelper
 
-  def like_sentence(count)
+  def like_sentence(count, type = 'post')
     return '' if count < 0
     case count
     when 0
-      "Nobody likes this post."
+      "Nobody likes this #{type}."
     when 1
-      "#{count} person likes this post."
+      "#{count} person likes this #{type}."
     else
-      "#{pluralize(count, 'person')} like this post."
+      "#{pluralize(count, 'person')} like this #{type}."
     end
   end
 
